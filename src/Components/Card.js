b3aa86ae5b716a8d09c8d286b39913920e2beb36
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-05-03 15:47:17 +0300

    Post initial files

---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import classes from './Card.module.css';
+
+const Card = () => {
+  return (
+    <div>
+      <h2>Improve your front-end skills by building projects</h2>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  );
+};
+
+export default Card;
