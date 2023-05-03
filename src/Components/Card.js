084cf930542a414cdab1c938654751b8b5b918f9
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-05-03 18:29:50 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,15 @@
+import Img from '../../src/image-qr-code.png';
+
 import classes from './Card.module.css';
 
 const Card = () => {
   return (
-    <div>
-      <h2>Improve your front-end skills by building projects</h2>
-      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    <div className={classes.card}>
+      <img src={Img}></img>
+      <div className={classes.text}>
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
     </div>
   );
 };
